e0751042072ec34da4fee7a3fc8689076e141af0
tmj99 <67132146+tmj99@users.noreply.github.com>
2023-12-25 16:56:31 +0000

    add camera module seperately

---
+++ cameragame.py
@@ -0,0 +1,45 @@
+import pygame
+from picamera2 import Picamera2
+import io
+
+# Initialize Pygame
+pygame.init()
+
+# Set the screen dimensions (adjust as needed)
+screen_width = 640
+screen_height = 480
+
+# Create a Pygame display window
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Create a Pygame clock to control frame rate
+clock = pygame.time.Clock()
+
+# Initialize the Raspberry Pi Camera
+camera = PiCamera2()
+camera.resolution = (screen_width, screen_height)
+
+try:
+    while True:
+        # Capture an image from the camera
+        stream = io.BytesIO()
+        camera.capture(stream, format='jpeg')
+        stream.seek(0)
+        image = pygame.image.load(io.BytesIO(stream.read()))
+
+        # Display the captured image on the Pygame screen
+        screen.blit(image, (0, 0))
+        pygame.display.flip()
+
+        # Check for quit events (e.g., closing the Pygame window)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        # Control the frame rate (adjust as needed)
+        clock.tick(30)
+
+except KeyboardInterrupt:
+    # Clean up and exit the program on KeyboardInterrupt (Ctrl+C)
+    pygame.quit()
