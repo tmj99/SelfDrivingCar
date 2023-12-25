dac6a40166f06df6e9b051c0f791327c8440fba2
tmj99 <67132146+tmj99@users.noreply.github.com>
2023-12-25 18:56:28 +0000

    camera resolution set to 1920×1080

--- cameratest.py
+++ cameratest.py
@@ -84,7 +84,7 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
 
 
 picam2 = Picamera2()
-picam2.resolution = picam2.MAX_RESOLUTION
+picam2.resolution = (1920×1080)
 picam2.configure(picam2.create_video_configuration(main={"size": picam2.resolution}))
 output = StreamingOutput()
 picam2.start_recording(JpegEncoder(), FileOutput(output))
