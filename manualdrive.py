131665284265688b3b8ae5ac5e196cd6fbbe7c34
tmj99 <67132146+tmj99@users.noreply.github.com>
2023-12-25 17:27:51 +0000

    hack together both stream and control
    The stream will unfortunately be viewed from a broswer, and controls
    will still be in the pygame module

--- manualdrive.py
+++ manualdrive.py
@@ -1,38 +1,108 @@
-# from picamera2 import Picamera2
-# import numpy as np
-# import cv2 as cv
-# import time
+"""
+This section enables the visual of the car to be streamed at
+https:// <ip of the pi> :8000/stream.mjpg
+"""
 
-# picam2 = Picamera2()
-# """figure out camera module"""
+#!/usr/bin/python3
 
-# cap = cv.VideoCapture(picam2)
+# Mostly copied from https://picamera.readthedocs.io/en/release-1.13/recipes2.html
+# Run this script, then point a web browser at http:<this-ip-address>:8000
+# Note: needs simplejpeg to be installed (pip3 install simplejpeg).
 
-# frame_number = 0
+import io
+import logging
+import socketserver
+from http import server
+from threading import Condition
 
-# while cap.isOpened():
-#     ret, frame = cap.read()
-    
-#     if ret == True:
+from picamera2 import Picamera2
+from picamera2.encoders import JpegEncoder
+from picamera2.outputs import FileOutput
 
-#         #  Test module
-#         cv.imshow('Frame', frame)
+PAGE = """\
+<html>
+<head>
+<title>picamera2 MJPEG streaming demo</title>
+</head>
+<body>
+<h1>Picamera2 MJPEG Streaming Demo</h1>
+<img src="stream.mjpg" width="640" height="480" />
+</body>
+</html>
+"""
 
-#         # cv.imshow('Blurred', blur(frame))
-#         #cv.imshow('Original', frame)
-#         frame_number += 1
-#         print(frame_number)
 
-#     if cv.waitKey(1) == ord('q'):
-#         break
+class StreamingOutput(io.BufferedIOBase):
+    def __init__(self):
+        self.frame = None
+        self.condition = Condition()
 
-# cap.release()
+    def write(self, buf):
+        with self.condition:
+            self.frame = buf
+            self.condition.notify_all()
 
-# cv.destroyAllWindows()
 
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
+
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+
+picam2 = Picamera2()
+picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
+output = StreamingOutput()
+picam2.start_recording(JpegEncoder(), FileOutput(output))
+
+try:
+    address = ('', 8000)
+    server = StreamingServer(address, StreamingHandler)
+    server.serve_forever()
+finally:
+    picam2.stop_recording()
 
 """
 This section is mainly for the controls of the car
+Will try to get the webstream to be able to get controls of the car
 """
 
 from Motor import *
