9e0d67ae5f000c13f18d2c44a04e21b86a6d0e9c
tmj99 <67132146+tmj99@users.noreply.github.com>
2023-12-29 23:12:57 +0000

    cvonlymacbookserver works too

--- cvonlymacbookserver.py
+++ cvonlymacbookserver.py
@@ -18,24 +18,33 @@ print('Listening at:',socket_address)
 
 fps,st,frames_to_count,cnt = (0,0,20,0)
 
-while True:
-	msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
-	print('GOT connection from ',client_addr)
+msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
+print('GOT connection from ',client_addr)
+print(msg)
+message = b'You are connected'
+server_socket.sendto(message,client_addr)
+
+while True:	
 	packet,_ = server_socket.recvfrom(BUFF_SIZE)
-	data = base64.b64decode(packet,' /')
-	npdata = np.fromstring(data,dtype=np.uint8)
-	frame = cv2.imdecode(npdata,1)
-	frame = cv2.putText(frame,'FPS: '+str(fps),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
-	cv2.imshow("RECEIVING VIDEO",frame)
-	key = cv2.waitKey(1) & 0xFF
-	if key == ord('q'):
-		server_socket.close()
-		break
-	if cnt == frames_to_count:
-		try:
-			fps = round(frames_to_count/(time.time()-st))
-			st=time.time()
-			cnt=0
-		except:
-			pass
-	cnt+=1
+	print(f'Length of packet: {len(packet)}')
+	try:
+		data = base64.b64decode(packet,' /')
+		print(f'Length of data: {len(data)}')
+		npdata = np.fromstring(data,dtype=np.uint8)
+		frame = cv2.imdecode(npdata,1)
+		frame = cv2.putText(frame,'FPS: '+str(fps),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+		cv2.imshow("RECEIVING VIDEO",frame)
+		key = cv2.waitKey(1) & 0xFF
+		if key == ord('q'):
+			server_socket.close()
+			break
+		if cnt == frames_to_count:
+			try:
+				fps = round(frames_to_count/(time.time()-st))
+				st=time.time()
+				cnt=0
+			except:
+				pass
+		cnt+=1
+	except:
+		print("Something went wrong")
