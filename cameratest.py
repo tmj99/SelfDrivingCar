fbcfe574aa6267620a5188f468d08664ac061f74
tmj99 <67132146+tmj99@users.noreply.github.com>
2023-12-29 12:45:32 +0000

    new index.html file

--- cameratest.py
+++ cameratest.py
@@ -12,7 +12,16 @@ video_config = picam2.create_video_configuration({"size": (1280, 720)})
 picam2.configure(video_config)
 encoder = H264Encoder(1000000)
 
-output1 = FfmpegOutput("-f mpegts udp://192.168.0.48:8000")
+# UDP Client-Server interaction
+UDP_IP          = "192.168.0.48"
+UDP_PORT        = 8000
+buffersize      = 1024
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+    sock.connect((UDP_IP, UDP_PORT))
+    stream = sock.makefile("wb")
+
+output1 = FileOutput(stream)
 output2 = FileOutput()
 encoder.output = [output1, output2]
 
@@ -24,8 +33,10 @@ time.sleep(5)
 # Start recording to a file.
 output2.fileoutput = "test.h264"
 output2.start()
+print("Recording to file in progress.")
 time.sleep(5)
 output2.stop()
+print("Recording stopped.")
 
 # The file is closed, but carry on streaming to the network.
 time.sleep(9999999)
