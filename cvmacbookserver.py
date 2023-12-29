610b4984dc39318444e21a101cbf4fc5eab93c31
tmj99 <67132146+tmj99@users.noreply.github.com>
2023-12-29 18:14:33 +0000

    testing with default viewer

--- cvmacbookserver.py
+++ cvmacbookserver.py
@@ -4,18 +4,18 @@ import socket
 import threading
 
 # Define the UDP server address and port
-UDP_IP          = "192.168.0.48"
-UDP_PORT        = 8000
-buffersize      = 1024
-msgFromServer   = "Connected to UDP Server"
-bytesToSend     = str.encode(msgFromServer)
+UDP_IP = "192.168.0.48"
+UDP_PORT = 8000
+buffersize = 1024
+msgFromServer = "Connected to UDP Server"
+bytesToSend = str.encode(msgFromServer)
 
 # Create a UDP socket
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_socket.bind((UDP_IP, UDP_PORT))
 
-# Create a global variable to store the latest video frame
-latest_frame = None
+# Initialize latest_frame with a blank image
+latest_frame = np.zeros((480, 640, 3), dtype=np.uint8)
 frame_lock = threading.Lock()  # Lock to protect access to latest_frame
 
 def receive_udp():
@@ -23,6 +23,7 @@ def receive_udp():
     connectMsg = False
     while True:
         data, addr = udp_socket.recvfrom(buffersize)  # Receive data
+        print(f"Received {len(data)} bytes from {addr}")  # Print status
         frame = cv2.imdecode(np.frombuffer(data, dtype=np.uint8), -1)
 
         with frame_lock:
@@ -39,11 +40,10 @@ def display_video():
         with frame_lock:
             frame = latest_frame
 
-        if frame is not None:
-            # Display the frame
-            cv2.imshow('UDP Video Stream', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        # Display the frame
+        cv2.imshow('UDP Video Stream', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     cv2.destroyAllWindows()
 
